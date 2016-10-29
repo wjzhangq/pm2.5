efd43d1b147272ca97e74cbac6528d23cb09a178
zhangwenjin <zhangwenjin@haodou.com>
2016-10-29 22:54:51 +0800

    kkk

--- pm2.py
+++ pm2.py
@@ -12,34 +12,38 @@ def my_date(unixtime, format='%m/%d/%Y %H:%M'):
     return d.strftime(format)
 
 if __name__ == '__main__':
-    uri = "http://api.yeelink.net/v1.0/device/351785/sensor/395611/datapoints"
+    uri = "http://api.yeelink.net/v1.0/device/351785/sensor/395612/datapoints"
     t = serial.Serial('/dev/ttyUSB0', 9600)
     s = struct.Struct('!16H')
-    pre_t = time.time()
-    start_t = pre_t
-    val = 0
-    while True:
-        n = t.inWaiting()
-        if n > 0:
-            buf = t.read(n)
-            if n == 32:
-                tmp = s.unpack(buf)
-                val = tmp[3]
+    start_time = time.time()
 
-            cur_t = time.time()
-            if cur_t - start_t > 40:
+    val_list = []
+    try:
+        while True:
+            n = t.inWaiting()
+            if n > 0:
+                buf = t.read(n)
+                if n == 32:
+                    tmp = s.unpack(buf)
+                    if tmp[3] > 0:
+                        val_list.append(tmp[3])
+
+            # 采集60s
+            if time.time() - start_time > 50:
                 break
+    except Exception, e:
+        print e
 
-            if val > 0 and cur_t - pre_t > 10:
-                str_t = my_date(cur_t, "%Y-%m-%dT%H:%M:%S")
-                buf = '{"timestamp":"%s","value":%s}' % (str_t, val)
-                val = 0
-                pre_t = cur_t
-                request = urllib2.Request(uri)
-                request.add_header(
-                    'U-ApiKey', '9e19146c5cc33233eb68fbe8c17ba559')
-                req = urllib2.urlopen(request, buf)
-                if req.code != 200:
-                    print req.code
-                print buf
-                print req.read()
+    c = len(val_list)
+    if c > 0:
+        str_t = my_date(time.time(), "%Y-%m-%dT%H:%M:%S")
+        buf = '{"timestamp":"%s","value":%s}' % (
+            str_t, sum(val_list) / c)
+        request = urllib2.Request(uri)
+        request.add_header(
+            'U-ApiKey', '9e19146c5cc33233eb68fbe8c17ba559')
+        req = urllib2.urlopen(request, buf)
+        if req.code != 200:
+            print req.code
+        print buf
+        print req.read()
